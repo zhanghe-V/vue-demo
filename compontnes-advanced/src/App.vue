1dd90a8336aa4e7fb4078e930e0c94d091e346eb
zhanghe-V <807597850@qq.com>
2018-09-27 22:34:42 +0800

    keepalive保持状态\slot插槽

---
+++ compontnes-advanced/src/App.vue
@@ -0,0 +1,69 @@
+<template>
+  <div class="container">
+  	<div class="row">
+  		<div class="col-xs-12">
+  			<!--
+  			一种不是非常好的解决办法是将quote当成一个属性传递到子组件中去
+  			子组件利用props进行接收并且显示，但是如果多行文本，格式文件等内容的存在，
+				这样操作就会变得十分的麻烦，所以这种模式的解决方案并非十分的理想
+  			-->
+
+  			<!-- <app-quote quote="A wonderful quote!"></app-quote> -->
+
+        <!-- slots的内容也可以引入data的变量内容 -->
+
+        <!-- 通过指定slot的名称，可以对应的操作slot显示的对象，
+        这样就可以将slot对象给拆分开来进行显示了 
+            如果去除p标签的slot属性，那么子组件中未命名的slot
+        也就成为了模认的slot对象-->
+  			<!-- <app-quote>
+  			<h2 slot="title">The Quote</h2>
+  			<h2 slot="titleFromData">{{quoteTitle}}</h2>
+  			<p slot="content">A wonderful quote!</p>
+  			<p> Not set slot name is default.111</p>
+  			<p> Not set slot name is default.222</p>
+  			</app-quote> -->
+
+        <button @click="selectedComponent = 'appQuote'">Quote</button>
+        <button @click="selectedComponent = 'appAuthor'">Author</button>
+        <button @click="selectedComponent = 'appNew'">New</button>
+        <hr>
+        <p>{{ selectedComponent }}</p>
+
+        <keep-alive>
+          <!-- 如果没有<keep-alive></keep-alive>进行激活状态的保持的话，
+          那么组件在切换的时候就会执行销毁操作 -->
+          <component :is="selectedComponent">
+            <p>Default Content</p>
+          </component>
+        </keep-alive>
+        
+
+  		</div>
+  	</div>
+  </div>
+</template>
+
+<script>
+import Quote from '@/components/Quote.vue';
+import Auther from '@/components/Auther.vue';
+import New from '@/components/New.vue';
+
+export default {
+  data:function(){
+    return {
+      quoteTitle:'The Quote',
+      selectedComponent:'appQuote'
+    }
+   },
+   components:{
+       appQuote:Quote,
+       appAuther:Auther,
+       appNew:New
+   }
+}
+</script>
+
+<style>
+
+</style>
---
+++ compontnes-advanced/src/components/Quote.vue
@@ -0,0 +1,44 @@
+<template>
+	<div>
+		<!-- <p>{{quote}}</p> -->
+
+		<!--只需要利用slot就可以将父组件中组件标签中的内容进行显示-->
+
+		<div class="title">
+			<slot name="title"></slot>
+		</div>
+		<div class="content">
+			<slot name="content"></slot>
+		</div>
+		<div class="titleFromData">
+			<slot name="titleFromData">titleFromData</slot>
+		</div>
+		
+		<!-- 如果父组件中没有定义slot的内容，那么在没有定义slot对象的时候，
+			如果在写slot内容以后将显示自己的slot内容，这是slot的默认值
+				要将 默认的slot对象与 slot的默认值加以区分
+			 -->
+
+		<span style="color:#ccc;">
+			<slot name="subtitle">The Subtitle</slot>
+		</span>
+		<slot></slot>
+		
+	</div>
+</template>
+
+<script>
+	export default{
+		// props:['quote']
+	}
+</script>
+
+<style scoped>
+	div {
+		border:1px solid #ccc;
+		box-shadow: 1px 1px 2px black;
+		padding: 30px;
+		margin:30px auto;
+		text-align: center;
+	}
+</style>
