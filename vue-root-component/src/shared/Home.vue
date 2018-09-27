2802596c273776b941e12f2e267fcc6f059a5448
zhanghe-V <807597850@qq.com>
2018-09-27 11:19:29 +0800

    啦

---
+++ vue-root-component/src/shared/Home.vue
@@ -0,0 +1,18 @@
+<template>
+  <h1>Home Vue Pag {{name}} <button @click='changeName'>Button</button></h1>
+  
+</template>
+
+<script>
+export default {
+    data:function(){
+        return {
+            name: 'Home Page'
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
